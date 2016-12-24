66b4adb55d8a44de3496325636330ad6ad4351fb
luke <llee@bodynbrain.com>
2016-12-24 13:53:02 -0700

    From Office

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>DCO Clocking-In</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+</head>
+
+<body onload="startTime()">
+<div class="container">
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">DCO Clocking-In</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Route::has('login'))
+                        <li> <a href="{{ url('/login') }}">Login</a> </li>
+                        <li> <a href="{{ url('/register') }}">Register</a> </li>
+                    @endif
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+    </nav>
+    @yield('mainTabs')
+
+
+    <!-- Main component for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h3>Current Time is <span id="txt"></span></h3>
+
+        <p> Please clock-in and out when you begin and end work; and when leave and return from meal times.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="{{url('/punchNow')}}" role="button"> Punch In & Out Now</a>
+        </p>
+    </div>
+    <div id="txt"></div>
+
+</div> <!-- /container -->
+
+
+
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script>
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        var date = today.getDate();
+        var month = today.getMonth();
+        var year = today.getFullYear();
+        var day = today.getDay();
+        m = checkTime(m);
+        s = checkTime(s);
+
+        var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        document.getElementById('txt').innerHTML =
+            h + ":" + m + ":" + s + " "+ months[month] +"/"+date+ "/"+ year + " " + "("+ days[day] +")";
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+</script>
+</body>
+</html>
