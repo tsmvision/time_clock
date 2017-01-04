589f900fd17af35ab04554e77228c111eee8a1a5
luke <llee@bodynbrain.com>
2017-01-04 15:31:17 -0700

    From Office

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-
         return User::create([
             'jjanID' => $data['jjanID'],
             'firstNm' => $data['firstNm'],
