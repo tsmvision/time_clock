353419311012672713de4bc1d5c3aa35bf104d0e
luke <llee@bodynbrain.com>
2017-01-05 09:34:39 -0700

    From Office

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,19 +47,20 @@
                                 @endif
                             </div>
                         </div>
-                    {{--
+
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-
+{{--
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">
                                     Forgot Your Password?
                                 </a>
+                                --}}
                             </div>
                         </div>
-                        --}}
+
                     </form>
                 </div>
             </div>
