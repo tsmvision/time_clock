38959323ebbbd0f54496eebafb5f4bb9ba54d9ce
luke <llee@bodynbrain.com>
2017-01-04 17:47:27 -0700

    From Office

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
                                 @endif
                             </div>
                         </div>
-
+                    {{--
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
@@ -59,6 +59,7 @@
                                 </a>
                             </div>
                         </div>
+                        --}}
                     </form>
                 </div>
             </div>
